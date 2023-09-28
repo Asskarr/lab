ca4cf3a9c07b01346d8b2b718a1f1e2cf1d022e2
nursu <askarsman2003@gmail.com>
2023-09-28 20:21:19 +0600

    aa

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StudentHouse studentHouse = new StudentHouse("kAZnu");
-        
-        StudentHouse studentHouse1 = new StudentHouse();
-        studentHouse1.setName("MUIT");
-    }
-}
